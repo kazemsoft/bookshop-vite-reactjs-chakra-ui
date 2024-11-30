520142a85f55105f898601475ba6684974f620b7
Kazem Gholian <kgholian@gmail.com>
2024-11-30 19:12:03 +0330

    fix build error

--- src/components/layouts/dashboard-layout/dashboard-layout.tsx
+++ src/components/layouts/dashboard-layout/dashboard-layout.tsx
@@ -1,10 +1,10 @@
 import { VStack } from "@chakra-ui/react";
 import useGetMe from "@hooks/queries/useGetMe";
-import { Outlet } from "react-router";
 import AppBar from "./AppBar/AppBar";
 
 export default function DashboardLayout() {
   const me = useGetMe();
+  console.log(me);
 
   return (
     <VStack w="full" h="full">
